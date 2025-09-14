bca04d774b5ac29bc01b9fcd8367ec52912ea44b
Shruti-2811 <shruti.22420156@viit.ac.in>
2025-09-14 19:26:26 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,60 @@
+import java.util.*;
+public class Addition {
+   public static void main(String arg[]){
+    Scanner sc=new Scanner(System.in);
+    /*System.out.println("Enter 1st value for addition");
+    int a=sc.nextInt();
+    System.out.println("Enter 2nd no. for addition");
+    int b=sc.nextInt();
+    int sum=a+b;
+    System.out.println(sum); */
+    /*area of circle
+    float radius=sc.nextFloat();
+    float area=3.14f*radius*radius;
+    System.out.println(area); */
+    /*char a= 'a';
+    char b= 'b';
+    System.out.println(b-a);
+     System.out.println(b); */
+     /*  Average of three numbers
+     int a= sc.nextInt();
+     int b=sc.nextInt();
+     int c= sc.nextInt();
+
+     int avg=(a+b+c)/3;
+     System.out.println(avg);
+   */
+      /* Area of a square
+      int side=sc.nextInt();
+      int area=side*side;
+      System.out.println(area);
+      */
+   /* Q.3 
+   System.out.println("Enter the price of pencil,pen,eraser");
+   float pc=sc.nextFloat(); 
+   float pe=sc.nextFloat();
+   float era=sc.nextFloat();
+   
+   float Total=(float)pc+pe+era;
+   System.out.println(Total);
+   //add on 18% tax
+
+   float newtoal=total+(0.18f*total);
+   System.out.println("Bill with 18% tax:"+newtotal);   
+      */
+   /* Q.4 
+   byte b=4;
+   char c='a';
+   short s=512;
+   int i=1000;
+   float f=3.14f;
+   double d=99.9954;
+   
+   double result=(f*b)+(i%c)-(d*s);
+   System.out.print(result);
+   */ 
+  int a=10;
+  int b=5;
+  System.out.println((a>=b)&&(9>4));
+   } 
+}
